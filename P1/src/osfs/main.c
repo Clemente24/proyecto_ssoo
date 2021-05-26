41c6d27a8df9c04a7cc1a2f8e4a566e336116b50
Jean-Paul Balazs <jnbalazs@uc.cl>
2021-05-25 22:42:27 -0400

    LEctura de disco

--- P1/src/osfs/main.c
+++ P1/src/osfs/main.c
@@ -13,17 +13,17 @@ int main(int argc, char **argv)
     
     // disk -> mbt->entradas[10][0] = 0b10001010;
 
-    //Testeo de lectura de disco real
-    //PAra comparar, se puede correr la linea hexdump -n100 simdiskformat.bin, y se puede ver que los bytes son los mismos
-    // int c;
-    // //Ponemos el puntero al inicio del archivo
-    // fseek(disk -> file_pointer,0, SEEK_SET);
-    // for (int i = 0; i <=100; i++){
-    //     c = fgetc(disk -> file_pointer);
-    //     printf("%x ", c); 
+    // Testeo de lectura de disco real
+    // PAra comparar, se puede correr la linea hexdump -n100 simdiskformat.bin, y se puede ver que los bytes son los mismos
+    int c;
+    //Ponemos el puntero al inicio del archivo
+    fseek(disk -> file_pointer,0, SEEK_SET);
+    for (int i = 0; i <=100; i++){
+        c = fgetc(disk -> file_pointer);
+        printf("%x ", c); 
 
-    // }
-    // printf("\n");
+    }
+    printf("\n");
 
     if (is_partition_valid(disk -> mbt, 10)){
         printf("Partition %i is valid", 10);
