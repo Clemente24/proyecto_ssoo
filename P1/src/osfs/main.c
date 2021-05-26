46d2911739a6d50c44de401d4af59de722645055
Jean-Paul Balazs <jnbalazs@uc.cl>
2021-05-25 23:14:59 -0400

    Write inside a disk.bin example

--- P1/src/osfs/main.c
+++ P1/src/osfs/main.c
@@ -16,14 +16,33 @@ int main(int argc, char **argv)
     // Testeo de lectura de disco real
     // PAra comparar, se puede correr la linea hexdump -n100 simdiskformat.bin, y se puede ver que los bytes son los mismos
     int c;
+
+    /* Forma para hacer iterando*/
+
+    //Ponemos el puntero al inicio del archivo
+    // fseek(disk -> file_pointer,0, SEEK_SET);
+    // for (int i = 0; i <=10; i++){
+
+    //     //Escribimos en el byte por byte
+    //     char byte_que_escribo = 0x02;
+    //     fseek(disk -> file_pointer,0, SEEK_SET);
+    //     fwrite(&byte_que_escribo, sizeof(char), 1, disk -> file_pointer);
+
+    //     //VEmos si se escribio bien
+    //     fseek(disk -> file_pointer,0, SEEK_SET);
+    //     c = fgetc(disk -> file_pointer);
+    //     printf("Al final: %x\n", c); 
+        
+
+    // }
+    // printf("\n");
+
+    /* Forma para hacer con array*/
     //Ponemos el puntero al inicio del archivo
     fseek(disk -> file_pointer,0, SEEK_SET);
-    for (int i = 0; i <=100; i++){
-        c = fgetc(disk -> file_pointer);
-        printf("%x ", c); 
-
-    }
-    printf("\n");
+    char array_de_bytes[6] = {0x80, 0x70, 0x80, 0x90, 0xaa};
+    // sprintf(array_de_bytes, "%x%x%x");
+    fwrite(array_de_bytes, sizeof(char), 6, disk -> file_pointer);
 
     if (is_partition_valid(disk -> mbt, 10)){
         printf("Partition %i is valid", 10);
