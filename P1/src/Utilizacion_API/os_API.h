64f47af8e4881c9c75485218f77033910a97a3c5
pcrodriguez <pcrodriguez@uc.cl>
2021-06-01 17:19:23 -0400

    init os-write

--- P1/src/Utilizacion_API/os_API.h
+++ P1/src/Utilizacion_API/os_API.h
@@ -1,7 +1,7 @@
 #include <stdio.h>  // FILE, fopen, fclose, etc.
 #include <string.h> // strtok, strcpy, etc.
 #include <stdlib.h> // malloc, calloc, free, etc.
-#include"directory.h"
+#include "directory.h"
 // Tells the compiler to compile this file once
 #pragma once
 
@@ -14,35 +14,34 @@ typedef struct mbt
 
 typedef struct disk
 {
-    char* name;
-    Mbt* mbt;
+    char *name;
+    Mbt *mbt;
     Directory directory;
-    FILE* file_pointer;
-  
+    FILE *file_pointer;
+
 } Disk;
 
-typedef struct osFILE {
+typedef struct osFILE
+{
     char name[28];
     unsigned int index_ptr;
     unsigned int directory_ptr;
     unsigned int size;
     unsigned int bytes_read;
-}osFILE;
-
+} osFILE;
 
 /* Variables globales */
 
-Mbt* mbt;
+Mbt *mbt;
 Disk *disk;
-unsigned char * particion_montada;
-
+unsigned char *particion_montada;
 
 // Functions
-void os_mount(char* diskname, int partition_id);
-Disk* init_disk(char* filename);
+void os_mount(char *diskname, int partition_id);
+Disk *init_disk(char *filename);
 
-// MBT functions 
-Mbt* init_mbt(FILE* fp);
+// MBT functions
+Mbt *init_mbt(FILE *fp);
 int is_partition_valid(int indice);
 int get_partition_id(int indice);
 int get_partition_index(int id);
@@ -54,13 +53,13 @@ int os_mbt();
 int os_create_partition(int id, int size);
 
 // funcion para leer archivos
-int os_read(osFILE* file_desc, void* buffer, int nbytes);
+int os_read(osFILE *file_desc, void *buffer, int nbytes);
 
 /*Función para ver si un archivo existe. Retorna 1 si el archivo existe
 y 0 en caso contrario. */
-int os_exists(char* filename);
+int os_exists(char *filename);
 //funcion para borrar archivo
-int os_rm(char* filename);
+int os_rm(char *filename);
 /*Función para listar los elementos de la partición actual. Imprime en pantalla los nombres de
 todos los archivos.*/
 void os_ls();
@@ -72,4 +71,7 @@ int available_directory(int ptr);
 int available_block();
 unsigned int file_data(unsigned int pt);
 
-osFILE * os_open(char * filename,char mode);
+osFILE *os_open(char *filename, char mode);
+
+/* Funcion para escribir archivos nuevos*/
+int os_write(osFILE *file_desc, void *buffer, int nbytes);
